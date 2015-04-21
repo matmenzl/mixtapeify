f2f1ad46fb6c52adf396f10f51149b39ed9ae3fb
Yaroslav Filyk <yarfilyk@gmail.com>
2015-04-21 19:07:55 +0300

    spotify_user updating fixed

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def spotify
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.update(spotify_user: RSpotify::User.new(request.env['omniauth.auth']).to_hash)
     sign_in_and_redirect @user
   end
 end
